15ed417ac9747b60953bea864cd3e166ed6ee756
nithyanand <nithyanandnadar.m@gmail.com>
2025-02-16 21:19:04 +0530

    profile page updating

--- src/pages/formar/Profile.jsx
+++ src/pages/formar/Profile.jsx
@@ -1,11 +1,110 @@
-import React from 'react'
+import React from "react";
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
+import { AvatarFallback } from "@/components/ui/avatar";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { Loader2 } from "lucide-react";
 
 const Profile = () => {
-  return (
-    <div className="my-24">
-      <h1>Profile</h1>
-    </div>
-  )
-}
 
-export default Profile
+    const isLoading = true;
+  return (
+    <div className="max-w-4xl mx-auto my-24 px-4">
+      <h1 className="font-bold text-2xl text-center md:text-left">Profile</h1>
+      <div className="flex flex-col md:flex-row items-center md:items-start gap-8 my-5">
+        <div className="flex flex-col items-center">
+          <Avatar className="h-24 w-24 md:h-32 md:w-32 ">
+            <AvatarImage
+              src="https://github.com/shadcn.png"
+              alt="@shadcn"
+              className=""
+            />
+            <AvatarFallback>CN</AvatarFallback>
+          </Avatar>
+        </div>
+        <div>
+          <div className="mb-2">
+            <h1 className="font-semibold text-gray-900 dark:text-gray-100">
+              Name:-
+              <span className="font-normal text-gray-700 dark:text-gray-300 ml-2">
+                Pashu Parni
+              </span>
+            </h1>
+          </div>
+          <div className="mb-2">
+            <h1 className="font-semibold text-gray-900 dark:text-gray-100">
+              Email:-
+              <span className="font-normal text-gray-700 dark:text-gray-300 ml-2">
+                Demo@gmail.com
+              </span>
+            </h1>
+          </div>
+          <div className="mb-2">
+            <h1 className="font-semibold text-gray-900 dark:text-gray-100">
+              Role:-
+              <span className="font-normal text-gray-700 dark:text-gray-300 ml-2">
+                Buyer \ Seller
+              </span>
+            </h1>
+          </div>
+          <Dialog>
+            <DialogTrigger asChild>
+              <Button size="sm" className="mt-2">
+                Edit Profile
+              </Button>
+            </DialogTrigger>
+            <DialogContent>
+              <DialogHeader>
+                <DialogTitle>Edit Profile</DialogTitle>
+                <DialogDescription>
+                  Make Changed to your profile here. Click save when you are
+                  done.
+                </DialogDescription>
+              </DialogHeader>
+              <div className="grid gap-4 py-4">
+                <div className="grid grid-cols-4 items-center gap-4">
+                  <Label>Name</Label>
+                  <Input type="text" placeholder="Please enter your name" className="col-span-3" />
+                </div>
+                <div className="grid grid-cols-4 items-center gap-4">
+                  <Label>Profile Photo</Label>
+                  <Input type="file" accept="image/*" className="col-span-3" />
+                </div>
+                            
+
+              </div>
+              <DialogFooter>
+          <Button disabled={isLoading}>{
+
+        isLoading ? (
+
+          <>
+          <Loader2 className="mr-2 h-4 w-4 animate-spin"/>
+          Please wait
+          </>
+
+ 
+):"Save Changes"
+
+  }</Button>
+                
+              </DialogFooter>
+            </DialogContent>
+          </Dialog>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
